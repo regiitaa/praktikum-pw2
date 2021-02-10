90ebf44883cb75ce776f3c3979a3140fca9f4597
regita <regitadwicahyani@student.untan.ac.id>
2021-02-10 15:42:15 +0700

    praktikum 3 - struktur kendali

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,18 @@
+<?php
+// Pengkondisian / Percabangan 
+// if else 
+// if else if else
+// Ternary
+// Switch
+
+$x = 2;
+if ( $x < 20 ) {
+    echo "Benar";
+}else if ( $x == 20 ) {
+    echo "Bingo!";
+
+} else {
+    echo "Salah";
+}
+
+?>
