e72915b71cd8201935dd9f4543f5cc6ff8a4229d
regita <regitadwicahyani@student.untan.ac.id>
2021-02-10 15:44:13 +0700

    praktikum 4 - function pada php

---
+++ pertemuan4/date.php
@@ -0,0 +1,22 @@
+<?php
+// Date
+// untuk menampilkan tanggal dengan format tertentu
+    // echo date("l, d-M-y");
+
+// Time
+// UNIX Timestamp / EPOCH time
+// Detik yang sudah berlalu sejak 1 Januari 1970
+
+// echo time ();
+// echo  date ("l, d M Y", time ()-60*60*24*100);
+
+// mktime
+// membuat sendiri detik 
+// mktime (0,0,0,0,0,0,)
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date ("l", mktime(0,0,0,12,18,2000));
+
+// Strtotime
+// echo date( "l, d M Y", strtotime ("18 december 2000"));
+
+?>
